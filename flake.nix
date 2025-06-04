4a76b37f1062140bd936c67e373a98de48f806ba
Suryakant <suryakant@Suryakants-MacBook-Air.local>
2025-06-05 01:22:25 +0530

    adds mac os setting

--- flake.nix
+++ flake.nix
@@ -1,31 +1,63 @@
 {
-  description = "My Ubuntu Nix";
+  description = "My system configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {
-    nixpkgs,
-    home-manager,
-    ...
-  }: let
-    # system = "aarch64-linux"; If you are running on ARM powered computer
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    homeConfigurations = {
-      surya = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+    let
+      configuration = { pkgs, ... }: {
+
+        # Disable nix-darwin's Nix management for Determinate compatibility
+        nix.enable = false;
+
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility. please read the changelog 
+        # before changing: `darwin-rebuild changelog`.
+        system.stateVersion = 4;
+
+        # The platform the configuration will be used on.
+        # If you're on an older system, replace with "x86_64-darwin"
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+        # Set primary user for homebrew and other user-specific options
+        system.primaryUser = "suryakant";
+
+        # Declare the user that will be running `nix-darwin`.
+        users.users.suryakant = {
+          name = "suryakant";
+          home = "/Users/suryakant";
+        };
+
+        security.pam.services.sudo_local.touchIdAuth = true;
+
+        # Create /etc/zshrc that loads the nix-darwin environment. 
+        programs.zsh.enable = true;
+        environment.systemPackages = [ pkgs.neofetch ];
+
+        homebrew = {
+          enable = true;
+          # onActivation.cleanup = "uninstall";
+
+          taps = [ ];
+          brews = [
+		"cowsay"
+	  ];
+          casks = [ "brave-browser" ];
+        };
+      };
+    in
+    {
+      darwinConfigurations."Suryakants-MacBook-Air" = nix-darwin.lib.darwinSystem {
         modules = [
-          ./home-manager/home.nix
+          configuration
         ];
       };
     };
-  };
 }
-
-
